0f926da4c586d02ff16afc02578d06069970c29f
akal-bytes <akalerupe@gmail.com>
2021-04-22 18:57:23 +0300

    project1

---
+++ src/project1.kt
@@ -0,0 +1,17 @@
+import kotlin.random.Random
+
+fun main() {
+bookAuthor()
+}
+fun bookAuthor() {
+    var author =" "
+    val african = Random.nextBoolean()
+    if (african){
+        author="Ngugi Wa Thiong'o"
+    }
+    if (!african){
+        author="John Steinbeck"
+    }
+    println("I will buy this book by $author when I go to town tomorrow.")
+
+}
